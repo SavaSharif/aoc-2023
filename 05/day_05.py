3572da9da9073097896268c17cbb5dc10f367ba2
Sava Sharif <s.sharif@fon-p.com>
2023-12-05 18:29:03 +0100

    working version

--- 05/day_05.py
+++ 05/day_05.py
@@ -1,46 +1,31 @@
-import re
 from typing import List
 from common.utils import read_file
 
 def p1(input: List[str]) -> int:
     seeds = [int(x) for x in input[0].split(" ")[1:]]
-    # skip a line
     input = input[2:]
 
-    # list of dicts
-    conversions = []
+    mappings = {}
     while input:
-        line = input.pop(0)
-        result = re.match(r"(\w+)-to-(\w+) map:", line)
-        # if result is not None, then we have a new category
-        if result:
-            # append a new dict to conversions and fill it with the next 3 value tuples until we hit an empty line
-            conversions.append({})
-            while input:
-                line = input.pop(0)
-                if line == "":
-                    break
-                source, destination, length = line.split(" ")
-                for i in range(int(length)):
-                    conversions[-1][int(destination) + i] = int(source) + i
-
-    seed_to_location = {}
-    # look up for each seed where it ends up, if there is no 
-    for seed in seeds:
-        location = seed
-        for conversion in conversions:
-            location = conversion.get(location, location)
-        seed_to_location[seed] = location
-    # print the lowest value
-    return min(seed_to_location.values())
-    
+        input = input[1:]
+        seeds = list(mappings.values()) if mappings else seeds # already initialize with the seeds
+        seed_to_soil = {seed: seed for seed in seeds}
         
-    
+        while input:
+            line = input.pop(0)
+            if line == "":
+                break
 
+            destination, source, length = [int(x) for x in line.split(" ")]
+            for i in range(length):
+                if source + i in seeds:
+                    seed_to_soil[source + i] = destination + i
 
+        mappings = seed_to_soil
 
-
-
+    # from the last dict, get the lowest value
+    lowest = min(mappings.values())
+    print(lowest)
 
 
 if __name__ == "__main__":
